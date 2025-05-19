5cf4219aee9c909131653da1e8e6dc769f63f138
Hritvik Gupta <hritvik7654@gmail.com>
2025-05-19 14:14:00 -0400

    Initial clean commit

--- main.py
+++ main.py
@@ -144,7 +144,7 @@ llm = Gemini(
     model="models/gemini-1.5-flash",
     api_key=GOOGLE_API_KEY,  
 )
-MODEL_ID = "gemini-2.0-flash"  # Latest version as of May 2025
+MODEL_ID = "models/gemini-2.0-flash"  # Latest version as of May 2025
 # vecto_chat = genai.GenerativeModel(MODEL_ID)
 gemini_model = Gemini(
     model=MODEL_ID,  # Add the "models/" prefix
